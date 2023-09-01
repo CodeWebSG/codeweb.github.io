3664caf9c6beac95a2b17530c967626a08bacf8a
cweitat <cweitat@gmail.com>
2023-09-01 12:55:49 +0800

    a

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ import { unifiedConditional } from 'unified-conditional'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/site',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
